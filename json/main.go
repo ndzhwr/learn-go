ffbec6df4b8de44abcb85dff1ef08882ad35a569
ndzhwr <ndizihiweregis06@gmail.com>
2023-04-18 19:38:06 +0200

    middlewares/v1

--- json/main.go
+++ json/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type User struct {
@@ -12,14 +13,26 @@ type User struct {
 	Password string `json:"password"`
 }
 
+func checkHeader(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token, ok := r.Header["Authorization"]
+		if !ok {
+			w.WriteHeader(406)
+			return
+		}
+		thetoken := strings.Split(token[0], " ")[1]
+		fmt.Printf("\n%q\n\n", thetoken)
+		f(w, r)
+	}
+}
 func main() {
-	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/decode", checkHeader(func(w http.ResponseWriter, r *http.Request) {
 		var user User
 		json.NewDecoder(r.Body).Decode(&user)
 		fmt.Printf("Username: %q , Email: %q, Password: %q \n", user.Username, user.Email, user.Password)
 
 		w.WriteHeader(200)
-	})
+	}))
 
 	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
 		user := User{
@@ -31,5 +44,5 @@ func main() {
 		w.WriteHeader(200)
 	})
 
-	http.ListenAndServe(":8080",nil);
+	http.ListenAndServe(":8080", nil)
 }
